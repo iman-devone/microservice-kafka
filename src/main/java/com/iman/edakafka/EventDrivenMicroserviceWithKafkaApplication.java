b8c98d2de634e69e8bd7401bc9607d10c6fb52e6
iman-devone <iman.eslami1983@gmail.com>
2024-01-30 18:05:01 +0100

    first commit

---
+++ src/main/java/com/iman/edakafka/EventDrivenMicroserviceWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.iman.edakafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenMicroserviceWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenMicroserviceWithKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iman/edakafka/EventDrivenMicroserviceWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iman.edakafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenMicroserviceWithKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
